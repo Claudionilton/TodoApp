0eed03afa823e17edb4836dac17910ba088d1331
Claudionilton <claudioniltonjose@gmail.com>
2023-07-17 17:33:36 -0300

    Created method save

--- app/src/main/java/controller/TaskController.java
+++ app/src/main/java/controller/TaskController.java
@@ -5,6 +5,7 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
@@ -19,6 +20,37 @@ public class TaskController {
     
             
             public void save(Task task) {
+                
+                String sql = "INSERT INTO tasks (idProject, "
+                        + "name, "
+                        + "descripition, "
+                        + "completed, "
+                        + "notes,"
+                        + "deadline,"
+                        + "createdAt,"
+                        + "updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+                
+                Connection connection = null;
+                PreparedStatement statement = null;
+                
+                try {
+                    connection = ConnectionFactory.getConnection();
+                    statement = connection.prepareStatement(sql);
+                    statement.setInt(1, task.getIdProject());
+                    statement.setString(2,task.getName());
+                    statement.setString(3, task.getDescription());
+                    statement.setBoolean(4, task.isIsCompleted());
+                    statement.setString(5, task.getNotes());
+                    statement.setDate(6, new Date(task.getCreatedAt().getTime()));
+                    statement.setDate(7, new Date(task.getCreatedAt().getTime()));
+                    statement.setDate(8, new Date(task.getUpdatedAt().getTime()));
+                    statement.execute();
+                    
+                } catch (Exception ex) {
+                    
+                    throw new RuntimeException("Erro ao salvar a tarefa "+ ex.getMessage(), ex);
+                    
+                }
                 
             }
             
