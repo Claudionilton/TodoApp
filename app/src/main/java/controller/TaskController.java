642b5a63eb1f168e2ab10863c7bce0a020b10819
Claudionilton <claudioniltonjose@gmail.com>
2023-07-17 22:15:57 -0300

    comments and corrections

--- app/src/main/java/controller/TaskController.java
+++ app/src/main/java/controller/TaskController.java
@@ -60,7 +60,7 @@ public class TaskController {
     public void update(Task task) {
 
         String sql = "UPDATE tasks SET"
-                + " idProject = ? , "
+                + "idProject = ? , "
                 + "name = ? ,"
                 + "description = ? ,"
                 + "notes = ? , "
@@ -72,11 +72,16 @@ public class TaskController {
 
         Connection connection = null;
         PreparedStatement statement = null;
-        
 
         try {
+            //Estabelecendo a conexão com o banco
             connection = ConnectionFactory.getConnection();
+            
+            //Preparando a query
             statement = connection.prepareStatement(sql);
+            
+            //Setando os valores do statement
+            
             statement.setInt(1, task.getIdProject());
             statement.setString(2, task.getName());
             statement.setString(3, task.getDescription());
@@ -85,9 +90,13 @@ public class TaskController {
             statement.setDate(6, new Date(task.getDeadline().getTime()));
             statement.setDate(7, new Date(task.getCreatedAt().getTime()));
             statement.setDate(8, new Date(task.getUpdatedAt().getTime()));
+            statement.setInt(9, task.getId());
+            
+            //Executando a query
             statement.execute();
+            
         } catch (Exception ex) {
-            throw new RuntimeException("Erro ao deletar a tarefa  " + ex.getMessage() , ex);
+            throw new RuntimeException("Erro ao atualizar a tarefa  " + ex.getMessage(), ex);
         }
 
     }
@@ -99,9 +108,16 @@ public class TaskController {
         PreparedStatement statement = null;
 
         try {
+            //Criação da conexão com o banco
             connection = ConnectionFactory.getConnection();
+            
+            //Preparando a query
             statement = connection.prepareStatement(sql);
+            
+            //Setando os valores
             statement.setInt(1, taskId);
+            
+            //Executando a query
             statement.execute();
         } catch (SQLException e) {
             throw new SQLException("Erro ao deletar a tarefa");
@@ -111,25 +127,31 @@ public class TaskController {
     }
 
     public List<Task> getAll(int idProject) {
-        
+
         String sql = "SELECT * FROM tasks WHERE idProject = ?";
-        
+
         Connection connection = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
-        
+
         //Lista de tarefas que é devolvida quando acontecer a chamada do metodo
         List<Task> tasks = new ArrayList<Task>();
-        
-        
+
         try {
+            //Conexão criada
             connection = ConnectionFactory.getConnection();
             statement = connection.prepareStatement(sql);
+            
+            //Setando o valor que corresponde ao filtro de busca
             statement.setInt(1, idProject);
+            
+            //Valor retornado pela execução da query
+            
             resultSet = statement.executeQuery();
             
+            //Enquanto houverem valores a serem percorridos no menu ResultSet
             while (resultSet.next()) {
-                
+
                 Task task = new Task();
                 task.setId(resultSet.getInt("id"));
                 task.setIdProject(resultSet.getInt("idProject"));
@@ -142,17 +164,15 @@ public class TaskController {
                 task.setUpdatedAt(resultSet.getDate("updatedAt"));
                 tasks.add(task);
             }
-            
-            
-            
+
         } catch (Exception ex) {
-            throw new RuntimeException("Erro ao deletar a tarefa" + ex.getMessage(), ex);
+            throw new RuntimeException("Erro ao inserir a tarefa" + ex.getMessage(), ex);
         } finally {
             ConnectionFactory.closeConnection(connection, statement, resultSet);
         }
-        
-        
-        return null;
+
+        //Lista de tarefas que foi criada e carregada do banco
+        return tasks;
     }
 
 }
